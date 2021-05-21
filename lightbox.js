14dba2652ba15acedf9d34852c0304585b2e830c
Marc Apesos <apesos2013@gmail.com>
2021-05-21 14:44:03 -0400

    Created a loop that displays the selected content based on the portfolio item clicked

---
+++ lightbox.js
@@ -0,0 +1,27 @@
+const lightbox = document.createElement("div");
+const portInfos = [...document.querySelectorAll(".port-info")];
+
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+
+//Adds click events to each picture
+const portItem = [...document.querySelectorAll(".port-item")];
+portItem.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    const target = e.target.id;
+    const portInfo = document.querySelector(`#${e.target.id}-content`);
+    console.log(portInfo);
+    lightbox.classList.add("active");
+    portInfo.classList.remove("display-none");
+    lightbox.appendChild(portInfo);
+  });
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target != lightbox) return;
+  portInfos.forEach((item) => {
+    item.classList.add("display-none");
+  });
+  lightbox.classList.remove("active");
+});
