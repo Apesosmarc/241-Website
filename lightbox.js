46ac4744f1ae5b30cb1b549db271fafba45f9679
Apesosmarc <73441355+Apesosmarc@users.noreply.github.com>
2021-07-29 12:58:14 -0400

    Heavy HTML & CSS refactor
    - deleted unused CSS classes and some attributes
    - Added styles to support new HTML classes
    
    HTML Validated via W3HTMLValidator
    - detailed Alt attributes added to all images
    - classes with same ID's fixed, and changed in lightbox.js
    - header elements added to each new section
    - nested UL best practices implemented
    - empty HTML attributed deleted
    - Headers added to each bulleted list

--- lightbox.js
+++ lightbox.js
@@ -1,16 +1,16 @@
-const lightbox = document.createElement("div");
 const portInfos = [...document.querySelectorAll(".port-info")];
 const exitMarks = [...document.querySelectorAll(".x")];
 
+const lightbox = document.createElement("div");
 lightbox.id = "lightbox";
 document.body.appendChild(lightbox);
 
 //Adds click events to each picture
-const portItem = [...document.querySelectorAll(".port-img-container")];
-portItem.forEach((item) => {
+const portImgContainers = [...document.querySelectorAll(".port-img-container")];
+portImgContainers.forEach((item) => {
   item.addEventListener("click", (e) => {
-    console.log(e.target.id);
-    const portInfo = document.querySelector(`#${e.target.id}-content`);
+    const targetId = item.id;
+    const portInfo = document.querySelector(`#${targetId}-content`);
     lightbox.classList.add("active");
     portInfo.classList.remove("display-none");
     lightbox.appendChild(portInfo);
